4f03bc5da55bf41e6587b190fc88fb899f4220e6
Jon Bee <j.bourim1@gmail.com>
2022-03-26 20:48:11 -0700

    [~] Re-exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,4 @@
-pub use caller_modpath_aggregator_functions;
-pub use caller_modpath_aggregator_macros;
+pub extern crate caller_modpath_aggregator_functions;
+pub extern crate caller_modpath_aggregator_macros;
+pub use caller_modpath_aggregator_functions::*;
+pub use caller_modpath_aggregator_macros::*;
